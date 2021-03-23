46b3ce1615bb0cc81fd4ba5edc3d13a6c12db264
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-03-23 21:36:32 +0000

    Recompiled for Windows

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,9 @@ int main()
     std::cout << "Please type in the name of the input file (include extension too): \n";
     std::cin >> fileName;
     std::ifstream inputFile(fileName);
-    if(!inputFile.good()) {
-        std::cout<<"File does not exist; exiting program\n";
+    if (!inputFile.good())
+    {
+        std::cout << "File does not exist; exiting program\n";
         return -1;
     }
     std::string target;
@@ -20,22 +21,29 @@ int main()
     std::cout << "Number of nodes: " << graph.get_node_size() << std::endl;
     std::cout << "Number of edges: " << graph.get_nr_edges() / 2 << std::endl;
 
-    std::cout << "Select which graph you would like to calculate distances: " << std::endl;
+    std::cout << "Select which node/vertex you would like to calculate distances for : " << std::endl;
     std::cin >> target;
     Node *source_node = graph.get_existing_node(target);
 
+    std::cout << "\n";
 
     int iters;
     std::cout << "How many iterations would you like the algorithm to run for? \n";
     std::cin >> iters;
 
+    std::cout << "\n";
+
     int fail_prob;
     std::cout << "Enter the probability that a link will fail at random? (0 - 100) \n";
     std::cin >> fail_prob;
 
+    std::cout << "\n";
+
     char split_horizon;
     std::cout << "Turn on split horizon functionality? (Y/n) \n";
     std::cin >> split_horizon;
+    std::cout << "\n";
+
     switch (split_horizon)
     {
     case 'Y':
@@ -46,7 +54,9 @@ int main()
         break;
     }
 
-    std::cout << "\n\n";
+    std::cout << "\n \n";
+    std::cout << "The program will terminate once you press any key...\n";
+    system("PAUSE");
 }
 
 void read_node_edges(std::ifstream &inputFile, Graph &graph)
